3c28556b23f68b6c0aa8a42a66da3cdaa9ce7f3a
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-07-26 04:14:56 -0400

    JEI pls

---
+++ src/main/java/com/tfar/anviltweaks/Jei.java
@@ -0,0 +1,22 @@
+package com.tfar.anviltweaks;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
+import net.minecraft.util.ResourceLocation;
+
+@JeiPlugin
+public class Jei implements IModPlugin {
+
+    @Override
+    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+      registration.addRecipeTransferHandler(RepairContainerv2.class, VanillaRecipeCategoryUid.ANVIL, 1, 9, 10, 36);
+    }
+
+    @Override
+    public ResourceLocation getPluginUid() {
+      return new ResourceLocation(AnvilTweaks.MODID, AnvilTweaks.MODID);
+    }
+
+  }
